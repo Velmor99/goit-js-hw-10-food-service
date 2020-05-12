9562ea8df9653c029dfb941039f9e092d9496c2e
velmor99 <volgagaz24100@gmail.com>
2020-05-12 08:26:49 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -6,19 +6,10 @@ import './styles.css';
 const menuMarcup = document.querySelector('.js-menu');
 const themeCheckbox = document.querySelector('.js-switch-input');
 const body = document.querySelector('body');
-const Theme = {
+const theme = {
 	LIGHT: 'light-theme',
 	DARK: 'dark-theme'
 };
-localStorage.setItem('settings', JSON.stringify(Theme));
-const localData = localStorage.getItem('settings')
-const parceData = JSON.parse(localData);
-
-
-console.log(localStorage);
-console.log(parceData);
-
-
 
 buildMenu(menu);
 
@@ -27,22 +18,22 @@ function buildMenu(menu) {
 	menuMarcup.insertAdjacentHTML('beforeend', marcup);
 }
 
-if(localStorage.theme === 'dark-theme') {
-    themeCheckbox.checked = true;
-    body.classList.add(parceData.DARK)
-} else if(localStorage.theme === 'light-theme') {
-    themeCheckbox.checked = false;
-    body.classList.add(parceData.LIGHT)
+if (localStorage.theme === 'dark-theme') {
+	themeCheckbox.checked = true;
+	body.classList.add(theme.DARK);
+} else if (localStorage.theme === 'light-theme') {
+	themeCheckbox.checked = false;
+	body.classList.add(theme.LIGHT);
 }
 
-themeCheckbox.addEventListener('click', e => {
-    if(themeCheckbox.checked === false) {
-        localStorage.setItem('theme', 'light-theme');
-        body.classList.add(parceData.LIGHT)
-        body.classList.remove('dark-theme')
-    } else if(themeCheckbox.checked === true) {
-        localStorage.setItem('theme', 'dark-theme');
-        body.classList.add(parceData.DARK)
-        body.classList.remove('light-theme');
-    }
-})
+themeCheckbox.addEventListener('click', (e) => {
+	if (themeCheckbox.checked === false) {
+		localStorage.setItem('theme', 'light-theme');
+		body.classList.add(theme.LIGHT);
+		body.classList.remove('dark-theme');
+	} else if (themeCheckbox.checked === true) {
+		localStorage.setItem('theme', 'dark-theme');
+		body.classList.add(theme.DARK);
+		body.classList.remove('light-theme');
+	}
+});
